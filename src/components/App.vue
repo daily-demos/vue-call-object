1c85a6f1878a8c8ffd27793ae690b118be7e2575
Jessica Mitchell <jess@pluot.co>
2021-10-05 14:56:58 -0400

    add header

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,25 +1,36 @@
 <template>
+  <Header />
   <Call />
 </template>
 
 <script>
-import Call from "./components/Call.vue";
+import Call from "./Call.vue";
+import Header from "./Header.vue";
 
 export default {
   name: "App",
   components: {
     Call,
+    Header,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+a {
+  text-decoration: none;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
+}
+body {
+  margin: 0;
 }
 </style>
