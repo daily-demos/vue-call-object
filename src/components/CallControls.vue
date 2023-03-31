f1cd405484d308274bafc3b973dfe57a5e8f871f
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2023-03-31 11:02:34 +0200

    Some internal shortcut tweaks

--- src/components/CallControls.vue
+++ src/components/CallControls.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="controls">
     <div class="devices">
-      <button title="ctrl+m / cmd+m" @click="handleAudioClick">
+      <button title="shift+m" @click="handleAudioClick">
         <template v-if="participant.audio">
           <img class="icon" :src="micOn" alt="" />
         </template>
@@ -10,7 +10,7 @@
         </template>
       </button>
 
-      <button title="ctrl+alt+v / cmd+option+v" @click="handleVideoClick">
+      <button title="shift+v" @click="handleVideoClick">
         <template v-if="participant.video">
           <img class="icon" :src="videoOn" alt="" />
         </template>
@@ -20,7 +20,11 @@
       </button>
 
       <template v-if="supportsScreenshare">
-        <button title="ctrl+alt+s / cmd+option+s" :disabled="disableScreenShare" @click="handleScreenshareClick">
+        <button
+          title="shift+s"
+          :disabled="disableScreenShare"
+          @click="handleScreenshareClick"
+        >
           <img class="icon" :src="screenShare" alt="" />
         </button>
       </template>
@@ -35,7 +39,7 @@
 <script>
 import daily from "@daily-co/daily-js";
 import { ref } from "vue";
-import { useMagicKeys, whenever } from '@vueuse/core';
+import { useMagicKeys, whenever } from "@vueuse/core";
 
 import leave from "../assets/leave_call.svg";
 import micOff from "../assets/mic_off.svg";
@@ -55,28 +59,21 @@ export default {
     "disableScreenShare",
   ],
   setup(props) {
-    const supportsScreenshare = ref(false)
+    const supportsScreenshare = ref(false);
 
     const {
-      cmd_m: audioToggleMAC,
-      ctrl_m: audioToggleWindows,
-      cmd_option_s: screenShareToggleMAC,
-      ctrl_alt_s: screenShareToggleWindows,
-      cmd_option_v: videoToggleMAC,
-      ctrl_alt_v: videoToggleWindows
-    } = useMagicKeys()
+      shift_m: audioToggle,
+      shift_s: screenShareToggle,
+      shift_v: videoToggle,
+    } = useMagicKeys();
 
-
-    whenever(audioToggleMAC, () => props.handleAudioClick())
-    whenever(audioToggleWindows, () => props.handleAudioClick())
-    whenever(screenShareToggleMAC, () => props.handleScreenshareClick())
-    whenever(screenShareToggleWindows, () => props.handleScreenshareClick())
-    whenever(videoToggleMAC, () => props.handleVideoClick())
-    whenever(videoToggleWindows, () => props.handleVideoClick())
+    whenever(audioToggle, () => props.handleAudioClick());
+    whenever(screenShareToggle, () => props.handleScreenshareClick());
+    whenever(videoToggle, () => props.handleVideoClick());
 
     return {
-      supportsScreenshare
-    }
+      supportsScreenshare,
+    };
   },
   data() {
     return {
